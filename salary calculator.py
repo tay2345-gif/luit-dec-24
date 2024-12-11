ae46137727ccdb8501a53b88dc6b78c015bf1145
tay2345-gif <TashuanLawrence7@gmail.com>
2024-12-11 18:25:23 -0500

    salary calculator

---
+++ salary calculator.py
@@ -0,0 +1,3 @@
+Hours_worked = float (input ("how many hours did you work last month ?"))
+Hourly_rate = float (input("what is your hourly rate ?"))
+print ("last month you earned",Hours_worked * Hourly_rate)
